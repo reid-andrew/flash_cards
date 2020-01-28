3366cd860c94028c4521f2c42e587be48dad3c44
Andrew Reid <areid.sj@gmail.com>
2020-01-27 18:32:06 -0700

    Write turn tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,13 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require '.lib/turn'
+require './lib/turn'
 
 class TurnTest < Minitest::Test
   def test_it_exists
     #skip
-    turn = Turn.new("Fairbanks", card)
+    turn = Turn.new("Juneau", card)
 
     assert_instance_of Turn, turn
   end
+
+  def test_it_has_a_guess
+    skip
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+  end
+
 end
