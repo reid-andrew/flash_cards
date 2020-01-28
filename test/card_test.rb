6e00baf5f2c9761b6bdbb543adb7952813977698
Andrew Reid <areid.sj@gmail.com>
2020-01-28 16:55:42 -0700

    Add additional card class tests

--- test/card_test.rb
+++ test/card_test.rb
@@ -18,6 +18,13 @@ class CardTest < Minitest::Test
     assert_equal "What is the capital of Alaska?", card.question
   end
 
+  def test_it_has_another_question
+    # skip
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    assert_equal "Which planet is closest to the sun?", card.question
+  end
+
   def test_it_has_an_answer
     # skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -25,10 +32,24 @@ class CardTest < Minitest::Test
     assert_equal "Juneau", card.answer
   end
 
+  def test_it_has_another_answer
+    # skip
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    assert_equal "Mercury", card.answer
+  end
+
   def test_it_has_a_category
     # skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal :Geography, card.category
+  end
+
+  def test_it_has_another_category
+    # skip
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    assert_equal :STEM, card.category
   end
 end
