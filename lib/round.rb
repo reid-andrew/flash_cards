6ab10627f730ccdfeaa244040d08139173416030
Andrew Reid <areid.sj@gmail.com>
2020-01-28 16:53:17 -0700

    Refactor and retest card class

--- lib/round.rb
+++ lib/round.rb
@@ -3,7 +3,7 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
-    # @number_correct = 0
+    @number_correct = 0
   end
 
   def deck
@@ -14,6 +14,9 @@ class Round
     new_turn = Turn.new(string, self.current_card)
     @turns << new_turn
     @deck.cards.rotate!(1)
+    if new_turn.correct?
+      @number_correct += 1
+    end
     return new_turn
   end
 
@@ -23,5 +26,25 @@ class Round
 
   def current_card
     @deck.cards[0]
+  end
+
+  def number_correct
+    @number_correct
+  end
+
+  def percent_correct
+    @number_correct.to_f / @turns.count
+  end
+
+  def number_correct_by_category(category)
+    @num_corr = 0
+    @turns.each do |turn|
+      if turn.correct? && turn.card.category == category
+        @num_corr += 1
+      end
+    end
+  end
+
+  def percent_correct_by_category(category)
   end
 end
