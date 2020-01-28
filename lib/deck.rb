3840b03aefe13d79f3aafb6260ab5cda5469fe78
Andrew Reid <areid.sj@gmail.com>
2020-01-27 18:55:44 -0700

    Define deck class

---
+++ lib/deck.rb
@@ -0,0 +1,23 @@
+class Deck
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def cards
+    @cards
+  end
+
+  def count
+    @cards.size
+  end
+
+  def cards_in_category(category)
+    @cards_in_category = []
+    @cards.select do |card|
+      @cards_in_category << card if card.category == category
+    end
+  end
+
+
+end
