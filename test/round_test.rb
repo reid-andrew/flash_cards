02e626e6dd95d6805475b5a111c501d9a0c19606
Andrew Reid <areid.sj@gmail.com>
2020-01-28 20:06:08 -0700

    Write test for number correct method

--- test/round_test.rb
+++ test/round_test.rb
@@ -171,6 +171,19 @@ class RoundTest < Minitest::Test
     assert_equal 1, round.number_correct
   end
 
+  def test_number_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 1, round.number_correct
+  end
+
   def test_percent_correct
     # skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
