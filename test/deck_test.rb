6d4581fde38d624d517ae48606036f908c6350dc
Andrew Reid <areid.sj@gmail.com>
2020-01-28 17:30:31 -0700

    Add cards in categories tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -41,7 +41,42 @@ class DeckTest < Minitest::Test
     assert_equal 3, deck.count
   end
 
-  def test_it_has_cards_in_category
-    skip
+  def test_it_has_cards_in_category_geography
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    # binding.pry
+
+    refute_equal [], deck.cards_in_category(:Geography)
+    assert_equal 1, deck.cards_in_category(:Geography).size
+  end
+
+  def test_it_has_cards_in_category_stem
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    # binding.pry
+
+    refute_equal [], deck.cards_in_category(:STEM)
+    assert_equal 2, deck.cards_in_category(:STEM).size
+  end
+
+  def test_it_has_cards_in_category_pop_culture
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    # binding.pry
+
+    assert_equal [], deck.cards_in_category("Pop Culture")
+    assert_equal 0, deck.cards_in_category("Pop Culture").size
   end
 end
