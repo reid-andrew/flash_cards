27d1f15f540a45309e52531c0c0ef013beeb7c11
Andrew Reid <areid.sj@gmail.com>
2020-01-28 19:50:02 -0700

    Write round class test percent correct by category test

--- test/round_test.rb
+++ test/round_test.rb
@@ -199,4 +199,19 @@ class RoundTest < Minitest::Test
 
     assert_equal 1, round.number_correct_by_category(:Geography)
   end
+
+  def test_percent_correct_by_category_geography
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+    round.take_turn("North north west")
+
+    assert_equal 100.0, round.percent_correct_by_category(:Geography)
+  end
 end
