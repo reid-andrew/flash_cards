f012557ef26d19a9ceba4bb94b6f4977c86c1b35
Andrew Reid <areid.sj@gmail.com>
2020-01-28 17:21:03 -0700

    Add test for deck class count method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,6 +14,8 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
   end
 
   def test_it_has_cards
@@ -29,7 +31,14 @@ class DeckTest < Minitest::Test
   end
 
   def test_it_has_a_count
-    skip
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
   end
 
   def test_it_has_cards_in_category
