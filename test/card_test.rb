ea8ed8f12685a182bdbb209b5c59a10b879c0a9b
Andrew Reid <areid.sj@gmail.com>
2020-01-29 15:42:20 -0700

    Refactor percent correct by category test

--- test/card_test.rb
+++ test/card_test.rb
@@ -13,42 +13,42 @@ class CardTest < Minitest::Test
   end
 
   def test_it_has_a_question
-    skip
+    # skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "What is the capital of Alaska?", card.question
   end
 
   def test_it_has_another_question
-    skip
+    # skip
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
     assert_equal "Which planet is closest to the sun?", card.question
   end
 
   def test_it_has_an_answer
-    skip
+    # skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "Juneau", card.answer
   end
 
   def test_it_has_another_answer
-    skip
+    # skip
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
     assert_equal "Mercury", card.answer
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal :Geography, card.category
   end
 
   def test_it_has_another_category
-    skip
+    # skip
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
     assert_equal :STEM, card.category
