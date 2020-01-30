5c22c53ac754e2b3ec5bb7b2a7bab9328aa397da
Andrew Reid <areid.sj@gmail.com>
2020-01-30 07:40:18 -0700

    Refactor card test suite to use setup method

--- test/card_test.rb
+++ test/card_test.rb
@@ -4,38 +4,37 @@ require './lib/card'
 
 class CardTest < Minitest::Test
 
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+  end
+
 
   def test_it_exists
     # skip
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    assert_instance_of Card, card
+    assert_instance_of Card, @card
+    assert_instance_of Card, @card2
   end
 
   def test_it_has_questions
     # skip
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
-    assert_equal "What is the capital of Alaska?", card.question
-    assert_equal "Which planet is closest to the sun?", card2.question
+    assert_equal "What is the capital of Alaska?", @card.question
+    assert_equal "Which planet is closest to the sun?", @card2.question
   end
 
   def test_it_has_answers
     # skip
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
-    assert_equal "Juneau", card.answer
-    assert_equal "Mercury", card2.answer
+    assert_equal "Juneau", @card.answer
+    assert_equal "Mercury", @card2.answer
   end
 
   def test_it_has_categories
     # skip
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
-    assert_equal :Geography, card.category
-    assert_equal :STEM, card2.category
+    assert_equal :Geography, @card.category
+    assert_equal :STEM, @card2.category
   end
 end
